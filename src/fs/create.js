602443e75c929fe22a45651f8fafba6e8f1b5e63
Mariia Mikhalchenko <m.mikhalchenko.de@gmail.com>
2025-04-28 09:31:07 +0200

    feat: implement file system operations

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import { writeFile } from 'fs/promises';
+
 const create = async () => {
-    // Write your code here 
+  // Write your code here
+  const content = 'I am fresh and young';
+  const fileName = 'fresh.txt';
+
+  try {
+    await writeFile(`src/fs/files/${fileName}`, content, { flag: 'wx' });
+    console.log(`File ${fileName} created successfully`);
+  } catch (err) {
+    if (err.code === 'EEXIST') {
+      throw new Error(`FS operation failed`);
+    } else {
+      throw err;
+    }
+  }
 };
 
 await create();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,29 @@
+import { rename as renameFile, access } from 'fs/promises';
+import { constants } from 'fs';
+
 const rename = async () => {
-    // Write your code here 
+  // Write your code here
+  const fileName = 'wrongFilename.txt';
+  const newFileName = 'properFilename.md';
+  const filePath = `src/fs/files/${fileName}`;
+  const newFilePath = `src/fs/files/${newFileName}`;
+
+  try {
+    await access(filePath, constants.F_OK);
+  } catch (err) {
+    throw new Error('FS operation failed');
+  }
+
+  try {
+    await access(newFilePath, constants.F_OK);
+    throw new Error('FS operation failed');
+  } catch (err) {
+    if (err.code !== 'ENOENT') {
+      throw err;
+    }
+  }
+  await renameFile(filePath, newFilePath);
+  console.log('File renamed successfully');
 };
 
 await rename();
