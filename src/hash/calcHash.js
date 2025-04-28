0ccd21447cda855dfe6408f05f4efc650c0a754d
Mariia Mikhalchenko <m.mikhalchenko.de@gmail.com>
2025-04-28 17:24:00 +0200

    feat: implement hash calculation

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+import { createHash } from 'crypto';
+import { createReadStream } from 'fs';
+import { pipeline } from 'stream/promises';
+
 const calculateHash = async () => {
-    // Write your code here 
+  // Write your code here
+  const filePath = 'src/hash/files/fileToCalculateHashFor.txt';
+  const readStream = createReadStream(filePath, { encoding: 'utf-8' });
+  const hash = createHash('sha256');
+
+  try {
+    await pipeline(readStream, hash);
+    console.log(hash.digest('hex'));
+  } catch (err) {
+    throw new Error('Hash calculation failed');
+  }
 };
 
 await calculateHash();
